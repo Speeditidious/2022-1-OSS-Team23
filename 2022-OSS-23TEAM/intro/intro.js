a6a8485623c19805bda258d3af5ae9e3c9e5cadd
joonseo2012 <joonseo2012@g.skku.com>
2022-05-19 23:19:55 +0900

    배경 img 실험1

--- 2022-OSS-23TEAM/intro/intro.js
+++ 2022-OSS-23TEAM/intro/intro.js
@@ -1,6 +1,7 @@
 
 let bg_images = ["Amumu_0.jpg", "Darius_0.jpg", "Fiora_0.jpg", "Garen_0.jpg","MonkeyKing_0.jpg","Sett_0.jpg","Warwick_0.jpg"]
 let background = document.querySelector("body");
+let idx = 0;
 
 //quick-menu
 let topBtn = document.getElementById("top_btn");
@@ -14,8 +15,22 @@ topBtn.addEventListener("click", () => {
 function shuffle(array) { array.sort(() => Math.random() - 0.5); }
 
 window.addEventListener("load", ()=>{
-    //images suffle
-    shuffle(bg_images);
-    
 
-})
+    shuffle(bg_images);
+    //background cycle
+    setTimeout(showImage,5000);
+});
+
+function showImage(){
+    //images suffle
+    idx = (idx+1)%(bg_images.length);
+    background.style.backgroundImage=`url(2022-OSS-23TEAM/intro/${bg_images[idx]})`;
+    setTimeout(showImage,5000);
+}
+
+function ch_bg()
+{   
+    idx = (idx+1)%(bg_images.length);
+    background.style.backgroundImage=`url(2022-OSS-23TEAM/intro/${bg_images[idx]})`;
+}
+
