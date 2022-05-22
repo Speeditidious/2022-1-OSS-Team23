942a1b8ca4782d84a020f7512b093ed423bfec1e
joonseo21 <joonseo2012@g.skku.edu>
2022-05-22 14:25:03 +0900

    background img fixed

--- 2022-OSS-23TEAM/intro/intro.js
+++ 2022-OSS-23TEAM/intro/intro.js
@@ -3,9 +3,10 @@ let bgImages = ["Amumu_0.jpg", "Darius_0.jpg", "Fiora_0.jpg", "Garen_0.jpg","Mon
 let bgLen = bgImages.length;
 let background = document.getElementById("background-intro");
 
-let ALTERING_TIMING0 = 8*60;
-let ALTERING_TIMING1 = 9*60;
-let ALTERING_TIMING2 = 10*60;
+let ALTERING_TIMING0 = 6*60;
+let ALTERING_TIMING1 = 7*60;
+let ALTERING_TIMING1_2 = 8*60;
+let ALTERING_TIMING2 = 9*60;
 
 let count = 120;
 let opa = 1;
@@ -46,11 +47,10 @@ let showImages = ()=>{
         opa = 1 + ((ALTERING_TIMING0-count) / 60);
         background.style.opacity = opa;
     }else if(count === ALTERING_TIMING1){
-        idx++;
-        if(idx === bgLen){idx = 0;}
+        idx = (idx+1) % bgLen;
         background.setAttribute("src",`2022-OSS-23TEAM/intro/${bgImages[idx]}`);
-    }else if(count > ALTERING_TIMING1 && count < ALTERING_TIMING2){
-        opa = ((count - ALTERING_TIMING1) / 60);
+    }else if(count > ALTERING_TIMING1_2 && count < ALTERING_TIMING2){
+        opa = ((count - ALTERING_TIMING1 - 60) / 60);
         background.style.opacity = opa;
     }else if(count === ALTERING_TIMING2){
         count = 0;
